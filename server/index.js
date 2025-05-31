3f4b90f4be20bcd31537a428670cb660663cfa52
ishankatiyar <ishankatiyar64@gmail.com>
2025-06-01 02:58:26 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://lms-app-client.onrender.com"],
     credentials: true
 }));
 
